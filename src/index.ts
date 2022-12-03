43c508636fca16e4030cdc811d9f33f2b1e67c17
hindicoder3 <112818265+hindicoder3@users.noreply.github.com>
2022-12-03 19:59:32 +0100

    small changes in builder

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Canvas, GlobalFonts } from "@napi-rs/canvas";
-import { join, dirname } from "path";
+import { join, dirname } from "node:path";
 export class WordGraphicBuilder {
   wordGuesses: string[];
   secretWord: string[];
@@ -30,17 +30,20 @@ export class WordGraphicBuilder {
     this.MISSING_COLOR = "#3a3a3c";
     this.UNKNOWN_COLOR = "#3a3a3c";
     this.TEXT_COLOR = "#ffffff";
-    this.wordGuesses = new Array();
+    this.wordGuesses = [];
     this.secretWord = Array.from(secretWord);
     const __dirname = dirname(new URL(import.meta.url).pathname);
-    GlobalFonts.registerFromPath(join(__dirname, "..", "fonts", this.FONT_NAME), "NS");
+    GlobalFonts.registerFromPath(
+      join(__dirname, "..", "fonts", this.FONT_NAME),
+      "NS"
+    );
   }
 
-  addWordGuesses(wordGuesses: string) {
+  addWordGuesses(wordGuesses: string): void {
     this.wordGuesses.push(wordGuesses);
   }
 
-  buildAsBufferedImage() {
+  buildAsBufferedImage(): Buffer {
     const canvas = new Canvas(356, 412);
     const ctx = canvas.getContext("2d");
     ctx.font = `${this.FONT_SIZE}px NS`;
@@ -50,33 +53,39 @@ export class WordGraphicBuilder {
     const gridHeight = this.BOX_PADDING_Y * 2 + this.BOX_SIZE;
     for (let row = 0; row < 6; row++) {
       for (let col = 0; col < 5; col++) {
-        const letterGuess = this.wordGuesses[row] ? this.wordGuesses[row][col] : null;
+        const letterGuess = this.wordGuesses[row]
+          ? this.wordGuesses[row][col]
+          : null;
         const startX = gridWidth * col + this.BOX_PADDING_X;
         const startY = gridHeight * row + this.BOX_PADDING_Y;
         const letterPosition = this.secretWord.indexOf(letterGuess);
         if (!letterGuess) {
           ctx.strokeStyle = this.UNKNOWN_COLOR;
           ctx.lineWidth = 2;
-          ctx.strokeRect(startX + 18, startY + 14, this.BOX_SIZE, this.BOX_SIZE);
+          ctx.strokeRect(
+            startX + 18,
+            startY + 14,
+            this.BOX_SIZE,
+            this.BOX_SIZE
+          );
+        } else if (letterPosition === -1) {
+          ctx.fillStyle = this.UNKNOWN_COLOR;
+          ctx.fillRect(startX + 18, startY + 14, this.BOX_SIZE, this.BOX_SIZE);
+        } else if (this.secretWord[col] === letterGuess) {
+          ctx.fillStyle = this.CORRECT_COLOR;
+          ctx.fillRect(startX + 18, startY + 14, this.BOX_SIZE, this.BOX_SIZE);
         } else {
-          if (letterPosition === -1) {
-            ctx.fillStyle = this.UNKNOWN_COLOR;
-            ctx.fillRect(startX + 18, startY + 14, this.BOX_SIZE, this.BOX_SIZE);
-          } else if (this.secretWord[col] === letterGuess) {
-            ctx.fillStyle = this.CORRECT_COLOR;
-            ctx.fillRect(startX + 18, startY + 14, this.BOX_SIZE, this.BOX_SIZE);
-          } else {
-            ctx.fillStyle = this.MISMATCH_COLOR;
-            ctx.fillRect(startX + 18, startY + 14, this.BOX_SIZE, this.BOX_SIZE);
-          }
+          ctx.fillStyle = this.MISMATCH_COLOR;
+          ctx.fillRect(startX + 18, startY + 14, this.BOX_SIZE, this.BOX_SIZE);
         }
-        if (letterGuess) {
-          const metrics = ctx.measureText(letterGuess.toUpperCase());
-          const textStartX = startX + 18 + (this.BOX_SIZE - metrics.width) / 2;
-          const textStartY = startY + 14 + (this.BOX_SIZE - this.FONT_SIZE) * 2.35;
-          ctx.fillStyle = this.TEXT_COLOR;
-          ctx.fillText(letterGuess.toUpperCase(), textStartX, textStartY);
-        }
+      }
+      if (letterGuess) {
+        const metrics = ctx.measureText(letterGuess.toUpperCase());
+        const textStartX = startX + 18 + (this.BOX_SIZE - metrics.width) / 2;
+        const textStartY =
+          startY + 14 + (this.BOX_SIZE - this.FONT_SIZE) * 2.35;
+        ctx.fillStyle = this.TEXT_COLOR;
+        ctx.fillText(letterGuess.toUpperCase(), textStartX, textStartY);
       }
     }
     return canvas.encode("png");
